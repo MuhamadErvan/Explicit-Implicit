f63f818457523527693130aa8ae1c15797e8f832
MuhamadErvan <62754564+MuhamadErvan@users.noreply.github.com>
2020-09-01 10:54:27 +0700

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.belajarexplicitimplicit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.belajarexplicitimplicit.IntentActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button explicitIntent;
+    Button implicitIntent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        explicitIntent = findViewById(R.id.explicitIntent);
+        explicitIntent.setOnClickListener(this);
+        implicitIntent = findViewById(R.id.implicitIntent);
+        implicitIntent.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.explicitIntent :
+                Intent explicit = new Intent(MainActivity.this, IntentActivity.class);
+                startActivity(explicit);
+                break;
+            case R.id.implicitIntent :
+                Intent implicit = new Intent(Intent.ACTION_VIEW, Uri.parse("https://1cak.com/"));
+                startActivity(implicit);
+                break;
+            default:
+                break;
+        }
+    }
+}
